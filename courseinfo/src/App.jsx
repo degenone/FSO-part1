6eb052b0861dd083444da620cb7799f53ce6cd6a
Tero Kilpeläinen <degenone1@gmail.com>
2023-10-02 18:41:49 +0300

    done exersice 1.4

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -20,41 +20,42 @@ const Content = (props) => {
     return (
         <div>
             {props.parts.map((part, i) => {
-                return <Part key={i} part={part} />
+                return <Part key={i} part={part} />;
             })}
         </div>
     );
 };
 
 const Total = (props) => {
+    const total = props.parts.reduce((acc, part) => (acc += part.exercises), 0);
     return (
         <>
-            <p>Number of exercises {props.total}</p>
+            <p>Number of exercises {total}</p>
         </>
     );
 };
 
 const App = () => {
     const course = 'Half stack application development';
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    };
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    };
-    const part3 = {
-        name: 'State of the component',
-        exercises: 14
-    };
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10,
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7,
+        },
+        {
+            name: 'State of the component',
+            exercises: 14,
+        },
+    ];
     return (
         <div>
             <Header course={course} />
-            <Content
-                parts={[part1, part2, part3]}
-            />
-            <Total total={part1.exercises + part2.exercises + part3.exercises} />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
     );
 };
