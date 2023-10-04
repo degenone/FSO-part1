1aebf396bea9acbb22338a28237bcbda13079548
Tero Kilpeläinen <degenone1@gmail.com>
2023-10-04 13:30:38 +0300

    exercise 1.6 done

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,12 +1,59 @@
 import { useState } from 'react';
 
-const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
+const Button = (props) => {
+    const { item, handleOnClick } = props;
 
-    return <div>code here</div>;
+    return (
+        <button
+            style={{ fontSize: '2.5rem' }}
+            onClick={handleOnClick}
+            title={item.title}>
+            {item.label}
+        </button>
+    );
+};
+//
+const App = () => {
+    const [reviews, setReviews] = useState([
+        { title: 'Good', count: 0, label: '🙂️' },
+        { title: 'Neutral', count: 0, label: '😐️' },
+        { title: 'Bad', count: 0, label: '🙁️' },
+    ]);
+
+    const handleReview = (idx) => {
+        return () => {
+            const newValue = [...reviews];
+            newValue[idx].count++;
+            setReviews(newValue);
+        };
+    };
+
+    return (
+        <div>
+            <h1>Give us feedback!</h1>
+            <div>
+                {reviews.map((item, i) => {
+                    return (
+                        <Button
+                            key={i}
+                            item={item}
+                            handleOnClick={handleReview(i)}
+                        />
+                    );
+                })}
+            </div>
+            <h2>Statistics:</h2>
+            <ul>
+                {reviews.map((review, i) => {
+                    return (
+                        <li key={i}>
+                            {review.title} {review.count}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
 
 export default App;
